b01f26c3e74917e065f4a74ad9f4abf0b3ac43f0
Nurkhan Kadirov <gameana7@gmail.com>
2026-03-22 00:00:49 +0500

    feat: added fallback for test pipeline

--- e2e-common/get-package-dir.js
+++ e2e-common/get-package-dir.js
@@ -5,12 +5,22 @@ const path = require('node:path');
  */
 function getPackageDir() {
     const packageArg = process.env.PACKAGE || process.argv.find(arg => arg.startsWith('--package='));
-    if (!packageArg) {
-        console.error('No package specified! Please pass --package=<packageDirName>');
+    const packageDirname = packageArg ? packageArg.split('=')[1] ?? packageArg : inferPackageFromCwd();
+    if (!packageDirname) {
+        console.error(
+            'No package specified! Please pass --package=<packageDirName> or run from packages/<packageDirName>.',
+        );
         process.exit(1);
     }
-    const packageDirname = packageArg.split('=')[1];
-    return path.join(__dirname, '../packages', packageDirname ?? packageArg, 'e2e');
+    return path.join(__dirname, '../packages', packageDirname, 'e2e');
+}
+
+function inferPackageFromCwd() {
+    const cwdParts = process.cwd().split(path.sep);
+    const packagesIndex = cwdParts.lastIndexOf('packages');
+    if (packagesIndex !== -1 && cwdParts.length > packagesIndex + 1) {
+        return cwdParts[packagesIndex + 1];
+    }
 }
 
 module.exports = { getPackageDir };
